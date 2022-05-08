7558bd880a41b90bdf7e7b4225ae2b26a5481362
Dayana Guerra <dayana96gm@gmail.com>
2022-05-08 16:26:51 -0300

    TG: updating json file and functions name

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -4,27 +4,23 @@ const dataTimeSheets = require('../data/time-sheets.json');
 
 const router = express.Router();
 
-router.get('/getAll', (req, res) => {
+router.get('/getAllTimeSheet', (req, res) => {
   res.send(dataTimeSheets);
 });
 
-router.delete('/deleteTs/:id', (req, res) => {
-    const timeSheetId = req.params.id;
-    const filterTs = dataTimeSheets.filter((timeSheet) =>{
-        return timeSheet.id !== timeSheetId;
-    })
-    if(dataTimeSheets.length === filterTs.length) {
-        res.send('Could not delete because the time sheet was not found')
-    }
-    else {
-        fs.writeFile('src/data/time-sheets.json', JSON.stringify(filterTs), (err) => {
-            if(err) {
-                res.send(err)
-            }
-            else {
-                res.send('Deleted correctly')
-            }
-        })
-    }
-})
+router.delete('/deleteTimeSheet/:id', (req, res) => {
+  const timeSheetId = req.params.id;
+  const filterTs = dataTimeSheets.filter((timeSheet) => timeSheet.id !== timeSheetId);
+  if (dataTimeSheets.length === filterTs.length) {
+    res.send('Could not delete because the time sheet was not found');
+  } else {
+    fs.writeFile('src/data/time-sheets.json', JSON.stringify(filterTs), (err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send('Deleted correctly');
+      }
+    });
+  }
+});
 module.exports = router;
