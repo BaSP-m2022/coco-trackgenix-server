740896e0d8c54f678bfd680647004f5528c7da5e
Ariana Maldonado <arii.maldonado97@gmail.com>
2022-05-09 23:38:25 -0300

    TG: post a new project

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -1,46 +1,55 @@
 const express = require('express');
-const fs = require('fs');
 const projects = require('../data/projects.json');
 
 const router = express.Router();
 
 // Create a Project - POST
-router.post('/add', (req, res) => {
-  const projectData = req.body;
-  if (projectData.name && projectData.description && projectData.clientName) {
-    projects.push(projectData);
-    fs.writeFile('src/data/projects.json', JSON.stringify(projects), (err) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send('Project created!');
-      }
-    });
+const generateId = () => {
+  let count = 0;
+  projects.forEach((project) => {
+    count += 1;
+    if (count !== project.id) {
+      return count;
+    }
+    return null;
+  });
+  count += 1;
+  return count;
+};
+
+router.post('/', (req, res) => {
+  const newProject = {
+    id: generateId(),
+    ...req.body,
+  };
+  const {
+    name,
+    description,
+    clientName,
+    startDate,
+    endDate,
+    devRate,
+    qaRate,
+    pmRate,
+    tiRate,
+    projectManager,
+    techLeader,
+    admin,
+  } = newProject;
+  if (name && description && clientName && startDate && endDate && devRate && qaRate
+        && pmRate && tiRate && projectManager && techLeader && admin) {
+    res.json(newProject);
+  } else {
+    res.json({ msg: 'Please, check your data is complete.' });
   }
 });
-// Modify a Project - PUT
-// router.put('/edit', (req, res) => {
 
-// server.put('api/memebers/:id', (req, res) => {
-//     const found = members.some(idFilter(req));
-//     if (found) {
-//     members.forEach((member, i) => {
-//     if (idFilter(req)(member)) {
-//     const updMember = {...member, ...req.body};
-//     members[i] = updMember
-//     res.json({ msg: 'Member updated', updMember });
-//     }
-//     });
-//     } else {
-//     res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
-//     }
-//     });
 // Get a Project - GET
-router.get('/getAll', (req, res) => {
+router.get('/', (req, res) => {
   res.send(projects);
 });
 
-router.get('/getProjectById/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const projectId = req.params.id;
   const selectedProject = projects.find((project) => project.id === projectId);
   if (selectedProject) {
