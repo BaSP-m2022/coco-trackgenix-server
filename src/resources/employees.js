36be60e45e9ad272afeeac47af63227ad93c0896
Lucas Rendo <lucasrendo1@gmail.com>
2022-05-10 12:09:49 -0300

    TG: Add features: Delete employee and filter by query params

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -31,7 +31,7 @@ router.get('/id/:id', (req, res) => {
   }
 });
 
-// filter list of employees
+// filter list of employees (missing filter for partial match)
 router.get('/filter', (req, res) => {
   const filters = {
     name: req.query.name,
@@ -39,26 +39,24 @@ router.get('/filter', (req, res) => {
     active: req.query.active,
   };
   const { name = '', lastName = '', active = '' } = filters;
-  let filteredList;
+  let filteredList = employees;
 
   if (name !== '') {
-    filteredList = employees.filter((employee) => employee.first_name === name);
+    // eslint-disable-next-line max-len
+    filteredList = filteredList.filter((employee) => employee.first_name.toLowerCase() === name.toLowerCase());
   }
   if (lastName !== '') {
-    filteredList = filteredList.filter((employee) => employee.last_name === lastName);
+    // eslint-disable-next-line max-len
+    filteredList = filteredList.filter((employee) => employee.last_name.toLowerCase() === lastName.toLowerCase());
   }
   if (active !== '') {
-    filteredList = filteredList.filter((employee) => employee.active === active);
+    // eslint-disable-next-line max-len
+    filteredList = filteredList.filter((employee) => employee.active.toString() === active.toLowerCase());
   }
-
-  if (name === '' && lastName === '' && active === '') {
-    res.json(employees);
-  } else {
-    res.json(filteredList);
-  }
+  res.json(filteredList);
 });
 
-// Create an employee
+// Create an employee (missing validation for existing information or wrong information)
 router.post('/create-employee', (req, res) => {
   const errors = [];
   const newEmployee = {
@@ -73,6 +71,7 @@ router.post('/create-employee', (req, res) => {
   const {
     first_name: firstName = '', last_name: lastName = '', phone = '', email = '', password = '',
   } = newEmployee;
+
   if (firstName === '' || lastName === '' || phone === '' || email === '' || password === '') {
     if (firstName === '') {
       errors.push({ msg: 'First Name is required' });
@@ -94,11 +93,64 @@ router.post('/create-employee', (req, res) => {
     employees.push(newEmployee);
     fs.writeFile('src/data/employees.json', JSON.stringify(employees), (err) => {
       if (err) {
-        res.send(err);
+        res.status(400).json(err);
       } else {
-        res.send('Employee Added');
+        res.json({ msg: 'Employee created', newEmployee });
       }
     });
+  }
+});
+
+// Edit an employee (no validations for wrong information)
+router.put('id/:id', (req, res) => {
+  const editEmployee = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    phone: req.body.phone,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  const {
+    first_name: firstName = '', last_name: lastName = '', phone = '', email = '', password = '',
+  } = editEmployee;
+  console.log('hello');
+  if (employees.some((employee) => employee.id === parseInt(req.params.id, 10))) {
+    const index = employees.findIndex((employee) => employee.id === parseInt(req.params.id, 10));
+    if (firstName !== '') {
+      employees[index].first_name = firstName;
+    }
+    if (lastName !== '') {
+      employees[index].last_name = lastName;
+    }
+    if (phone !== '') {
+      employees[index].phone = phone;
+    }
+    if (email !== '') {
+      employees[index].email = email;
+    }
+    if (password !== '') {
+      employees[index].password = password;
+    }
+    res.json({ msg: 'Employee updated' });
+  } else {
+    res.status(404).json({ msg: `Member with id ${req.params.id} not found` });
+  }
+});
+
+// Delete an employee
+router.delete('/id/:id', (req, res) => {
+  if (employees.some((employee) => employee.id === parseInt(req.params.id, 10))) {
+    const deleted = employees.find((employee) => employee.id === parseInt(req.params.id, 10));
+    const updatedList = employees.filter((employee) => employee.id !== parseInt(req.params.id, 10));
+    fs.writeFile('src/data/employees.json', JSON.stringify(updatedList), (err) => {
+      if (err) {
+        res.status(400).json(err);
+      } else {
+        res.json({ msg: 'Employee Deleted', deleted });
+      }
+    });
+  } else {
+    res.status(404).json({ msg: `Member with id ${req.params.id} not found` });
   }
 });
 
