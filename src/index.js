5cc146904d4178a6b584b9bf7f252ce395d17b83
axelfiol <axel.fiol97@hotmail.com>
2022-05-10 16:37:02 -0300

    BaSP-W08-2022: finished endpoint to update a superAdmin

--- src/index.js
+++ src/index.js
@@ -3,27 +3,26 @@
 const express = require('express');
 
 // use "require" to import JSON files
-const admins = require('./data/admins.json');
-
 const app = express();
 const port = process.env.PORT || 3000;
+const superAdminsData = require('./data/super-admins.json');
 const superAdmins = require('./resources/super-admins');
-
+const bodyParser = require('body-parser');
+// configure the app to use bodyParser()
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.use(bodyParser.json());
+app.use('/superAdmins', superAdmins);
 app.get('/', async (req, res) => {
   res.send('Hello World!');
 });
-
-app.get('/superAdmins', superAdmins.getAllSuperAdmins);
-
-app.get('/superAdmins/:id', superAdmins.getOneSuperAdmin);
-
 app.get('/admins', (req, res) => {
   res.status(200).json({
     data: admins,
   });
 });
-
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
+// eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}`);
 });
--- src/resources/super-admins.js
+++ src/resources/super-admins.js
@@ -1,19 +1,112 @@
 const superAdmins = require('../data/super-admins.json');
+const express = require('express');
 
-module.exports = {
-  getAllSuperAdmins(req, res) {
-    res.status(200).json({
-      data: superAdmins,
+const router = express.Router();
+const fs = require('fs');
+const res = require('express/lib/response');
+
+router.use((req, res, next) => {
+  console.log('Time: ', Date.now());
+  next();
+});
+
+router.post('/', (req, res) => {
+  const newSuperAdmin = {
+    id: superAdmins.length + 1,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    phone: req.body.phone,
+    email: req.body.email,
+    password: req.body.password,
+    active: true,
+  };
+  const {
+    firstName,
+    lastName,
+    phone,
+    email,
+    password,
+  } = newSuperAdmin;
+  superAdmins.push(newSuperAdmin);
+  if (firstName && lastName && phone && email && password) {
+    fs.writeFile('src/data/super-admins.json', JSON.stringify(superAdmins), (err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send({
+          msg: 'Superadmin created successfully',
+          created_superadmin: newSuperAdmin,
+        });
+      }
     });
-  },
+  } else {
+    res.json({ msg: 'Please, check your data is complete' });
+  }
+});
 
-  getOneSuperAdmin(req, res) {
+router.get('/', (req, res) => {
+  res.status(200).json({
+    data: superAdmins,
+  });
+});
+
+router.get('/:id', (req, res) => {
+  const found = superAdmins.some((superAdmin) => superAdmin.id === parseInt(req.params.id, 10));
+  if (found) {
+    res.json(superAdmins.filter((superAdmin) => superAdmin.id === parseInt(req.params.id, 10)));
+  } else {
+    res.status(400).json({ msg: `No superAdmin with the id of ${req.params.id}` });
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  const found = superAdmins.some((superAdmin) => superAdmin.id === parseInt(req.params.id, 10));
+  if (found) {
+    const newSuperAdmins = (superAdmins.filter((superAdmin) => superAdmin.id !== parseInt(req.params.id, 10)));
+    const deletedSuperAdmin = (superAdmins.filter((superAdmin) => superAdmin.id === parseInt(req.params.id, 10)));
+    fs.writeFile('src/data/super-admins.json', JSON.stringify(newSuperAdmins), (err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send({
+          msg: 'Deleted successfully',
+          deleted_superadmin: deletedSuperAdmin,
+        });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No superAdmin with the id of ${req.params.id}` });
+  }
+});
+
+router.put('/:id', (req, res) => {
     const found = superAdmins.some((superAdmin) => superAdmin.id === parseInt(req.params.id, 10));
-
     if (found) {
-      res.json(superAdmins.filter((superAdmin) => superAdmin.id === parseInt(req.params.id, 10)));
-    } else {
-      res.status(400).json({ msg: `No superAdmin with the id of ${req.params.id}` });
+        const updatedSuperAdmin = {
+            id: req.params.id.length + 1,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            phone: req.body.phone,
+            email: req.body.email,
+            password: req.body.password,
+            active: true,
+          };
+        superAdmins.forEach((superAdmin, i) => {
+            if (superAdmin.id == req.params.id) {
+                superAdmins[i] = updatedSuperAdmin;
+            }
+        }); 
+        
+        fs.writeFile('src/data/super-admins.json', JSON.stringify(superAdmins), (err) => {
+            if (err) {
+              res.send(err);
+            } else {
+              res.send({
+                msg: 'Updated successfully',
+                updated_superadmin: updatedSuperAdmin,
+              });
+            }
+        });
     }
-  },
-};
+});
+module.exports = router;
