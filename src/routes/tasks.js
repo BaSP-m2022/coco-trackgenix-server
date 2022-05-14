d1bafffd240e5cd181ea51001ba066aad1e9470b
Lucas Rendo <lucasrendo1@gmail.com>
2022-05-14 16:58:19 -0300

    TG-13-19-25: Add validations, update tasks schema and generate tasks routes

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,5 +1,14 @@
 import express from 'express';
+import tasksController from '../controllers/tasks';
+import taskValidation from '../validations/tasks';
 
 const router = express.Router;
+
+router
+  .get('/', tasksController.getTasks)
+  .get('/:id', tasksController.getTaskById)
+  .post('/', taskValidation.validateCreation, tasksController.createTask)
+  .delete('/:id', tasksController.deleteTask)
+  .put('/:id', taskValidation.validateUpdate, tasksController.updateTask);
 
 export default router;
