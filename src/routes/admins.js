29f63d47f52a154419e82694a32b1ec2298a89d5
Dayana Guerra <dayana96gm@gmail.com>
2022-05-15 22:22:23 -0300

    TG-24: adding validation for admin creation

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import adminsController from '../controllers/admins';
-// import adminsValidation from '../validations/admins';
+import adminsValidation from '../validations/admins';
 
 const router = express.Router();
 
@@ -8,13 +8,7 @@ router
   .get('/', adminsController.getAllAdmins)
   .get('/:id', adminsController.getAdminById)
   .delete('/:id', adminsController.deleteAdmin)
-  .post('/', adminsController.createAdmin)
+  .post('/', adminsValidation.validateAdminCreation, adminsController.createAdmin)
   .put('/:id', adminsController.updateAdmin);
 
 export default router;
-
-//     .get('/',adminsController.getAllAdmins)
-//     .post('/', adminsValidation.validateAdminCreation, adminsController.createAdmin)
-//     .get('/:id', adminsController.getAdminById)
-//     .put('/:id', adminsController.updateAdmin)
-//     .delete('/:id', adminsController.deleteAdmin)
