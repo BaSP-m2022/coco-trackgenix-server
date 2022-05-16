ce1df49c9d1392ec537fde4a9d64e6a9256a0883
lucianofrailelopez <lucianofrailee@gmail.com>
2022-05-16 03:08:54 -0300

    TG-16: Delete and Post

--- src/controllers/super-admins.js
+++ src/controllers/super-admins.js
@@ -12,22 +12,66 @@ const getAllSuperAdmins = async (req, res) => {
 };
 
 const getSuperAdminById = async (req, res) => {
-    try {
-      if (req.params.id) {
-        const superAdmin = await superAdminModel.findById({ _id: req.params.id });
-        return res.status(200).json(superAdmin);
-      }
+  try {
+    if (req.params.id) {
+      const superAdmin = await superAdminModel.findById({ _id: req.params.id });
+      return res.status(200).json(superAdmin);
+    }
+    return res.status(400).json({
+      msg: 'missing id parameter',
+    });
+  } catch (error) {
+    return res.json({
+      msg: error,
+    });
+  }
+};
+
+const deleteSuperAdmin = async (req, res) => {
+  try {
+    if (!req.params.id) {
       return res.status(400).json({
         msg: 'missing id parameter',
       });
-    } catch (error) {
-      return res.json({
-        msg: error,
+    }
+    const result = await superAdminModel.findByIdAndDelete({ _id: req.params.id });
+    if (!result) {
+      return res.status(404).json({
+        msg: 'The admin has not been found',
       });
     }
-  };
+    return res.status(200).json({
+      msg: 'The admin has been successfully deleted',
+    });
+  } catch (error) {
+    return res.json({
+      msg: 'An error has ocurred',
+    });
+  }
+};
+
+const createSuperAdmin = async (req, res) => {
+  try {
+    const superAdmin = await superAdminModel.create({
+      name: req.body.name,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+      active: req.body.active,
+    });
+    const resultSuperAdmin = await superAdmin.save();
+    return res.status(201).json(resultSuperAdmin);
+  } catch (error) {
+    return res.json({
+      msg: 'An error has occurred',
+      error: error.details[0].message,
+    });
+  }
+};
 
 export default {
   getAllSuperAdmins,
-  getSuperAdminById
+  getSuperAdminById,
+  deleteSuperAdmin,
+  createSuperAdmin,
 };
--- src/routes/super-admins.js
+++ src/routes/super-admins.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import superAdminsController from '../controllers/super-admins';
+import superAdminsValidation from '../validations/super-admins';
 
 const router = express.Router();
 
 router
   .get('/', superAdminsController.getAllSuperAdmins)
-  .get('/:id', superAdminsController.getSuperAdminById);
+  .get('/:id', superAdminsController.getSuperAdminById)
+  .delete('/:id', superAdminsController.deleteSuperAdmin)
+  .post('/', superAdminsValidation.validateSuperAdminCreation, superAdminsController.createSuperAdmin);
 
 export default router;
