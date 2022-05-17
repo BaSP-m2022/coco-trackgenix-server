27d5e0f54eb5f4af36715582c191e9d5cfbf26f7
RNDOSKO <Ricardo.dosko@gmail.com>
2022-05-16 21:50:30 -0300

    Update validations

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import projects from '../controllers/projects';
+import projectsValidation from '../validations/projects';
 
 const router = express.Router();
 
 router
   .get('/:id', projects.getProjectById)
   .delete('/:id', projects.deleteProject)
-  .put('/:id', projects.updateProject);
+  .put('/:id', projectsValidation.updateValidate, projects.updateProject);
+
 export default router;
