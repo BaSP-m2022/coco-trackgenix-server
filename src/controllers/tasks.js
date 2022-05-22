85145d711d7511ab3c40b59171d2645492e1813a
Ariana Maldonado <arii.maldonado97@gmail.com>
2022-05-22 19:47:18 -0300

    BaSP-TG-56: adding fixes for test

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -28,13 +28,13 @@ const getTaskById = async (req, res) => {
       });
     }
   } catch (error) {
-    if (error.value) { // the server recieved the data
+    if (error.value) {
       res.status(404).json({
         msg: `Code 404: Task with id ${req.params.id} not found`,
         data: undefined,
         error: false,
       });
-    } else { // the server did not receieve the data
+    } else {
       res.status(500).json({
         msg: 'Code 500: There was an error',
         data: error,
--- src/seeds/tasks-seeds.js
+++ src/seeds/tasks-seeds.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 // Generate ObjectId on https://observablehq.com/@hugodf/mongodb-objectid-generator
 export default [{
   _id: mongoose.Types.ObjectId('62891835d4e286802a02756e'),
-  decription: 'task decription',
+  decription: 'task description',
   workedHours: '20',
-  date: '2021-12-05T03:00:00.000+00:00',
 }];
