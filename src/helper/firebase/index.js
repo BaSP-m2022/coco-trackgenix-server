d40f42a68bda00b1db86ef866d18fae4373219f2
Matias <vadalamati@gmail.com>
2022-06-27 19:05:02 -0300

    TG-157: Modified routes to the '.env' resources

--- src/helper/firebase/index.js
+++ src/helper/firebase/index.js
@@ -3,9 +3,9 @@ import 'dotenv/config';
 
 const firebaseConfig = {
   credential: admin.credential.cert({
-    privateKey: process.env.REACT_APP_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    clientEmail: process.env.REACT_APP_FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
 };
 
