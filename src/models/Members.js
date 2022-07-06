b542b6f4c3d79291526586752034930520159bc9
gregorioelias <gregorio.elias24@gmail.com>
2022-07-06 17:23:48 -0300

    TG-194:modified members Schema

--- src/models/Members.js
+++ src/models/Members.js
@@ -2,13 +2,17 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-const MemberSchema = new Schema({
-  employee: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Employee',
-  }],
-  role: { type: String, required: true, enum: ['DEV', 'QA', 'PM', 'TL'] },
-  rate: { type: Number, required: true },
-}, { timestamps: true });
+const MemberSchema = new Schema(
+  {
+    employee: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Employee',
+      required: true,
+    },
+    role: { type: String, required: true, enum: ['DEV', 'QA', 'PM'] },
+    rate: { type: Number, required: true },
+  },
+  { timestamps: true },
+);
 
 export default mongoose.model('Members', MemberSchema);
